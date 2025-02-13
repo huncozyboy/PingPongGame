b0754548aa4c13823f432a60c4fe30ccde980613
huncozyboy <leejh5838@naver.com>
2025-02-14 04:59:06 +0900

    feat : 전역 예외처리 핸들러 구현

---
+++ src/main/java/prography/pingpong/global/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package prography.pingpong.global.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import prography.pingpong.global.common.response.ApiResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final int SERVER_ERROR_CODE = 500;
+    private static final String SERVER_ERROR_MESSAGE = "에러가 발생했습니다.";
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException() {
+        ApiResponse<Void> response = ApiResponse.response(SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE, null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
