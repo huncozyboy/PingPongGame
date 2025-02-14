0e1aeb03a44a3ad7f29d8c7a3a57a22b33429548
huncozyboy <leejh5838@naver.com>
2025-02-15 06:52:03 +0900

    feat : UserGetService에서 getAllUsers 메서드 구현

---
+++ src/main/java/prography/pingpong/domain/user/service/UserGetService.java
@@ -0,0 +1,27 @@
+package prography.pingpong.domain.user.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import prography.pingpong.domain.user.dto.response.UserResponse;
+import prography.pingpong.domain.user.entity.User;
+import prography.pingpong.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserGetService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Page<UserResponse> getAllUsers(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id"));
+        Page<User> users = userRepository.findAll(pageable);
+
+        return users.map(UserResponse::from);
+    }
+}
