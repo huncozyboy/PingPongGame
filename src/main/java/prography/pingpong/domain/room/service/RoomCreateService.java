0090e733340b29d44ac8c8fa77c0873092385680
huncozyboy <leejh5838@naver.com>
2025-02-16 05:01:05 +0900

    feat : 방 생성 메서드 createRoom 서비스단에서 구현

---
+++ src/main/java/prography/pingpong/domain/room/service/RoomCreateService.java
@@ -0,0 +1,34 @@
+package prography.pingpong.domain.room.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import prography.pingpong.domain.exception.InvalidRequestException;
+import prography.pingpong.domain.room.dto.request.RoomCreateRequest;
+import prography.pingpong.domain.room.entity.Room;
+import prography.pingpong.domain.room.repository.RoomRepository;
+import prography.pingpong.domain.user.service.UserGetService;
+import prography.pingpong.domain.userRoom.service.UserRoomService;
+
+@Service
+@RequiredArgsConstructor
+public class RoomCreateService {
+
+    private final UserGetService userGetService;
+    private final UserRoomService userRoomService;
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public void createRoom(RoomCreateRequest request) {
+
+        userGetService.findActiveUser(request.userId());
+
+        if (userRoomService.isUserAlreadyInRoom(request.userId())) {
+            throw new InvalidRequestException();
+        }
+
+        Room room = Room.create(request.title(), request.userId(), request.roomType());
+
+        roomRepository.save(room);
+    }
+}
