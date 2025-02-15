aa2f48cb9b98002f3f5dd1cf61e0a06bacb11c78
huncozyboy <leejh5838@naver.com>
2025-02-16 04:58:56 +0900

    feat : 활성화 상태인 유저 조회 findByIdAndStatus 메서드 추가

--- src/main/java/prography/pingpong/domain/user/repository/UserRepository.java
+++ src/main/java/prography/pingpong/domain/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package prography.pingpong.domain.user.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import prography.pingpong.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByIdAndStatus(int id, User.Status status);
 }
