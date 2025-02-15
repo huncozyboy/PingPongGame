ecc1f06d46bd07e115c4ee2b10503b813bfe612c
huncozyboy <leejh5838@naver.com>
2025-02-16 05:02:18 +0900

    feat : getAllRooms 메서드로 전체 방 조회 메서드 서비스 단에서 구현

---
+++ src/main/java/prography/pingpong/domain/room/service/RoomGetService.java
@@ -0,0 +1,27 @@
+package prography.pingpong.domain.room.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import prography.pingpong.domain.room.dto.response.RoomResponse;
+import prography.pingpong.domain.room.entity.Room;
+import prography.pingpong.domain.room.repository.RoomRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RoomGetService {
+
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public Page<RoomResponse> getAllRooms(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id"));
+        Page<Room> rooms = roomRepository.findAll(pageable);
+
+        return rooms.map(RoomResponse::from);
+    }
+}
