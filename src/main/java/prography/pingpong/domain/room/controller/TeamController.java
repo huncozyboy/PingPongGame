50d645e81a803c33efd38dbf90ca16592b5387f1
huncozyboy <leejh5838@naver.com>
2025-02-16 11:52:42 +0900

    feat : 팀 변경 컨트롤러단에서 구현

---
+++ src/main/java/prography/pingpong/domain/room/controller/TeamController.java
@@ -0,0 +1,29 @@
+package prography.pingpong.domain.room.controller;
+
+import static prography.pingpong.global.common.response.ResponseMessage.SUCCESS_RESPONSE;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import prography.pingpong.domain.room.dto.request.RoomRequest;
+import prography.pingpong.domain.room.service.TeamChangeService;
+import prography.pingpong.global.common.response.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/team")
+public class TeamController {
+
+    private final TeamChangeService teamChangeService;
+
+    @Operation(summary = "팀 변경")
+    @PutMapping("/{roomId}")
+    public ApiResponse<Void> changeTeam(@PathVariable int roomId, @RequestBody RoomRequest request) {
+        teamChangeService.teamChange(request, roomId);
+        return ApiResponse.response(SUCCESS_RESPONSE.getCode(), SUCCESS_RESPONSE.getMessage());
+    }
+}
