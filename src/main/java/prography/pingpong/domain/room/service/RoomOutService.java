2f7b7787e1bbdd6081ff24c01f0af635c2912b7b
huncozyboy <leejh5838@naver.com>
2025-02-16 10:34:00 +0900

    feat : 방 나가기 RoomOutService 구현

---
+++ src/main/java/prography/pingpong/domain/room/service/RoomOutService.java
@@ -0,0 +1,51 @@
+package prography.pingpong.domain.room.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import prography.pingpong.domain.exception.InvalidRequestException;
+import prography.pingpong.domain.room.dto.request.RoomOutRequest;
+import prography.pingpong.domain.room.entity.Room;
+import prography.pingpong.domain.room.repository.RoomRepository;
+import prography.pingpong.domain.user.service.UserGetService;
+import prography.pingpong.domain.userRoom.repository.UserRoomRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RoomOutService {
+
+    private final UserGetService userGetService;
+    private final RoomRepository roomRepository;
+    private final UserRoomRepository userRoomRepository;
+
+    @Transactional
+    public void outRoom(RoomOutRequest request, int roomId) {
+
+        userGetService.findActiveUser(request.userId());
+
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(InvalidRequestException::new);
+
+        if (!userRoomRepository.existsByUserIdAndRoomId(request.userId(), roomId)) {
+            throw new InvalidRequestException();
+        }
+
+        if (room.isInProgressOrFinished()) {
+            throw new InvalidRequestException();
+        }
+
+        if (room.getHost().equals(request.userId())) {
+            userRoomRepository.deleteByRoomId(roomId);
+            room.finish();
+            roomRepository.save(room);
+            return;
+        }
+
+        userRoomRepository.deleteByUserIdAndRoomId(request.userId(), roomId);
+
+        if (userRoomRepository.countByRoomId(roomId) == 0) {
+            room.finish();
+            roomRepository.save(room);
+        }
+    }
+}
