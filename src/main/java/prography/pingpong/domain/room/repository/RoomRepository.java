80add2505060e383e4ca5f47c71a35088592ea6a
huncozyboy <leejh5838@naver.com>
2025-02-16 09:21:50 +0900

    feat : WAIT상태인 방 조회 위해서 findByIdAndStatus 메서드 방레포에 추가

--- src/main/java/prography/pingpong/domain/room/repository/RoomRepository.java
+++ src/main/java/prography/pingpong/domain/room/repository/RoomRepository.java
@@ -1,9 +1,11 @@
 package prography.pingpong.domain.room.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import prography.pingpong.domain.room.entity.Room;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    Optional<Room> findByIdAndStatus(Integer roomId, Room.Status status);
 }
